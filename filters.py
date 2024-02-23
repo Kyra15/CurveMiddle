a5b1a4a878e206d47af2adcbf9fe47ba7824e6bc
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-02-23 15:16:56 -0500

    Update filters.py

--- filters.py
+++ filters.py
@@ -3,16 +3,6 @@ import cv2
 import numpy as np
 
 
-# function that created a mask on an image given the coordinates of the mask
-# and returns the masked image
-def masking(img, vertices):  # define mask for area of interest
-    mask = np.zeros_like(img)
-    match_mask_color = 255
-    cv2.fillPoly(mask, vertices, match_mask_color)
-    masked_image = cv2.bitwise_and(img, mask)
-    return masked_image
-
-
 # applies a grayscale filter, gaussian blur, and canny image filter on a frame given and returns the final frame
 def filters(img):
     gray_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -21,3 +11,13 @@ def filters(img):
     cannyed_image = cv2.Canny(blur, 250, 300, apertureSize=5)
     cv2.imshow("cann", cannyed_image)
     return cannyed_image
+
+
+# function that created a mask on an image given the coordinates of the mask
+# and returns the masked image
+def masking(img, vertices):  # define mask for area of interest
+    mask = np.zeros_like(img)
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
